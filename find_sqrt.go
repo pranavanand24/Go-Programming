c55e5bbcef11c46bdf126ea03fc1f9cad8696426
Pranav Anand <pranavanand2410@gmail.com>
2020-05-16 19:10:18 +0530

    Add files via upload

---
+++ find_sqrt.go
@@ -0,0 +1,10 @@
+package main
+
+import ("fmt"
+	"math")
+
+func main(){
+	fmt.Println("The square root of 4 is", math.Sqrt(4))
+}
+
+
---
+++ genr_rand_no.go
@@ -0,0 +1,8 @@
+package main
+
+import ("fmt"
+		"math/rand")
+
+func main(){
+	fmt.Println("A number from 1-50",rand.Intn(50))
+}
